3f0b4976caa725ae88313d853ef079dde825ea9f
Vinícius Silva Santos <vinicius01012@gmail.com>
2024-01-31 18:32:15 -0300

    finish challenge tunts rocks

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # Rocks_Challenge
- tunts rocks dev training challenge
+Tunts Rocks Dev Training Challenge
+
+## Student Situation Updater
+This Node.js project utilizes the Express framework and Google Sheets API to update the academic situation of students based on their attendance, grades, and calculated averages. The script reads data from a specified Google Sheet, performs calculations, and updates the sheet with the final results.
+
+### Setup
+Before running the project, make sure you have the following prerequisites:
+
+#### Google API Credentials:
+1. Create a project on the Google Cloud Console.
+2. Enable the Google Sheets API for your project.
+3. Create credentials and download the JSON file. Rename it to `credentials.json` and place it in the root directory of the project.
+
+#### Node.js and npm:
+- Make sure you have Node.js and npm installed on your machine.
+
+### Install Dependencies:
+Open a terminal in the project's root directory and run:
+
+```bash
+npm install
+```
+
+### Configure Spreadsheet
+Open index.js and replace the spreadsheetId variable with the ID of your target Google Sheet.
+
+## Running the Project
+To start the server, run the following command in the terminal:
+
+```bash
+npm start
+```
+
+The server will run on port 3001. Access it by navigating to http://localhost:3001 in your browser.
+
+## API Endpoint
+POST /updateSituation:
+This endpoint triggers the update of student situations in the specified Google Sheet. The sheet's structure is assumed to have relevant data starting from the 4th row. The script calculates averages, updates approval status, and adds a final grade (if applicable).
+
+### Important Notes
+- Ensure the Google Sheet contains valid and consistent data, and its structure matches the assumptions made in the script.
+- Any errors encountered during the process will be logged to the console and result in a 500 Internal Server Error response.
+- Feel free to modify the script or adapt it to your specific use case. If you encounter any issues, refer to the error messages in the console for troubleshooting.
+
+## How to Get in Touch
+
+I'm always open to chat and collaborate on interesting projects. You can reach out to me through the following channels:
+
+- Email: vinicius01012@gmail.com
+- LinkedIn: [https://www.linkedin.com/in/_viniciussis](URL)
+- GitHub: [https://github.com/viniciussis](URL)
+
+Feel free to contact me if you have any questions, ideas, or collaboration opportunities!
+
+## Contributing
+
+Feel free to contribute or open issues for suggestions and bug fixes. Pull requests are welcome!
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+---
+**Developed by Vinícius Silva Santos in partnership with Tunts Rocks**
--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const express = require("express");
-const req = require("express/lib/request");
 const { google } = require("googleapis");
 
 const app = express();
 app.use(express.json());
 
 async function getAuthSheets() {
+  // Create authentication client using credentials.json file
   const auth = new google.auth.GoogleAuth({
     keyFile: "credentials.json",
     scopes: "https://www.googleapis.com/auth/spreadsheets",
   });
 
+  // Obtain client instance
   const client = await auth.getClient();
 
+  // Create Google Sheets API instance
   const googleSheets = google.sheets({
     version: "v4",
     auth: client,
   });
 
+  // Spreadsheet ID for the target Google Sheet
   const spreadsheetId = "14lZ5NBwJya97BvEZAOTXKsBTXrbNPWW-2KMbSD9DRk0";
 
   return {
@@ -28,127 +31,57 @@ async function getAuthSheets() {
   };
 }
 
-app.get("/metadata", async (req, res) => {
-  const { googleSheets, auth, spreadsheetId } = await getAuthSheets();
-
-  try {
-    const metadata = await googleSheets.spreadsheets.get({
-      auth,
-      spreadsheetId,
-    });
-
-    res.send(metadata.data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Erro ao requisitar dados!");
-  }
-});
-
-app.get("/getRows", async (req, res) => {
-  const { googleSheets, auth, spreadsheetId } = await getAuthSheets();
-
-  try {
-    const getRows = await googleSheets.spreadsheets.values.get({
-      auth,
-      spreadsheetId,
-      range: "engenharia_de_software",
-    });
-    res.send(getRows.data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Erro ao requisitar dados!");
-  }
-});
-
-app.get("/getRow/:rowNumber", async (req, res) => {
-  const { googleSheets, auth, spreadsheetId } = await getAuthSheets();
-
-  const rowNumber = req.params.rowNumber;
-  const range = `engenharia_de_software!A${rowNumber}:Z${rowNumber}`;
-
-  try {
-    const getRow = await googleSheets.spreadsheets.values.get({
-      auth,
-      spreadsheetId,
-      range: range,
-    });
-
-    res.send(getRow.data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Erro ao requisitar dados!");
-  }
-});
-
-app.get("/getCells/:rowNumber", async (req, res) => {
-  const { googleSheets, auth, spreadsheetId } = await getAuthSheets();
-
-  const rowNumber = req.params.rowNumber;
-  const range = `engenharia_de_software!D${rowNumber}:F${rowNumber}`;
-
-  try {
-    const getCells = await googleSheets.spreadsheets.values.get({
-      auth,
-      spreadsheetId,
-      range: range,
-    });
-
-    const values = getCells.data.values[0]; // Obtém os valores da primeira (e única) linha
-
-    if (!values || values.length < 3) {
-      res.status(500).send("Não há dados suficientes para calcular a média.");
-      return;
-    }
-
-    // Converte os valores para números e calcula a média
-    const numericValues = values.map(parseFloat);
-    const average =
-      numericValues.reduce((acc, val) => acc + val, 0) /
-      (numericValues.length * 10);
-    const roundedAverage = parseFloat(average.toFixed(2));
-
-    res.send({ values: numericValues, average: roundedAverage });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Erro ao requisitar dados!");
-  }
-});
-
 app.post("/updateSituation", async (req, res) => {
+  // Retrieve necessary objects from getAuthSheets function
   const { googleSheets, auth, spreadsheetId } = await getAuthSheets();
 
   try {
+    // Fetch data from the "engenharia_de_software" sheet
     const getRows = await googleSheets.spreadsheets.values.get({
       auth,
       spreadsheetId,
       range: "engenharia_de_software",
     });
 
+    // Extract rows from the fetched data
     const rows = getRows.data.values;
 
-    for (let i = 1; i < rows.length; i++) {
-      const faltas = parseInt(rows[i][2]);
+    // Check if the data is valid
+    if (!rows || rows.length < 3) {
+      throw new Error("Invalid data retrieved from the spreadsheet");
+    }
+
+    // Iterate through rows starting from index 3 (4th row)
+    for (let i = 3; i < rows.length; i++) {
+      // Parse values from the row
+      const absences = parseInt(rows[i][2]);
       const p1 = parseInt(rows[i][3]);
       const p2 = parseInt(rows[i][4]);
       const p3 = parseInt(rows[i][5]);
 
-      const media = (p1 + p2 + p3) / 3;
+      // Check for invalid numeric data
+      if (isNaN(absences) || isNaN(p1) || isNaN(p2) || isNaN(p3)) {
+        throw new Error("Invalid numeric data in the spreadsheet");
+      }
 
-      if (faltas > 0.25 * 60) {
+      // Calculate the average
+      const average = (p1 + p2 + p3) / 3 / 10;
+
+      // Update row based on conditions
+      rows[i][7] = 0;
+      if (absences > 0.25 * 60) {
         rows[i][6] = "Reprovado por Falta";
-        rows[i][7] = "";
-      } else if (media < 5) {
-        rows[i][6] = "Reprovado";
-        rows[i][7] = "";
-      } else if (media >= 5 && media < 7) {
-        rows[i][6] = "Final";
-        rows[i][7] = "";
+      } else if (average < 5) {
+        rows[i][6] = "Reprovado por Nota";
+      } else if (average < 7) {
+        rows[i][6] = "Exame Final";
+        rows[i][7] = Math.ceil(10 - average); //The formula ensures that the final grade (m + naf) / 2 is at least 5, following mathematical principles.
       } else {
         rows[i][6] = "Aprovado";
-        rows[i][7] = "";
       }
     }
 
+    // Update the sheet with the modified rows
     await googleSheets.spreadsheets.values.update({
       auth,
       spreadsheetId,
@@ -157,11 +90,15 @@ app.post("/updateSituation", async (req, res) => {
       resource: { values: rows },
     });
 
-    res.send("Situação atualizada com sucesso.");
+    // Log success message and send response to the client
+    console.log("Student situation updated successfully");
+    res.status(200).send("Student situation updated successfully");
   } catch (error) {
-    console.error(error);
-    res.status(500).send("Erro ao atualizar a situação.");
+    // Log error message and send internal server error response
+    console.error("Error:", error.message);
+    res.status(500).send("Internal Server Error");
   }
 });
 
-app.listen(3001, () => console.log("Rodando na porta 3001"));
+// Start the server on port 3001
+app.listen(3001, () => console.log("The server is running on port 3001"));
